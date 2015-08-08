e5476dfade57f1d6ec0448e7962d39018aadc954
Dean Lunz <deanlunz@createdbyx.com>
2015-08-08 14:20:21 -0700

    removed core code files and moved them into there own repo located at https://bitbucket.org/createdbyx/entitas-simplified-code-only Core code files have been added back into repo as a sub module

--- Entitas.Unity Core Library/Assets/Entitas/Libraries/Entitas/Interfaces/IMultiReactiveSystem.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Entitas {
-    public interface IMultiReactiveSystem : IReactiveExecuteSystem {
-        IMatcher[] triggers { get; }
-        GroupEventType[] eventTypes { get; }
-    }
-}
-
--- Entitas.Unity Core Library/Assets/Entitas/Libraries/Entitas/Interfaces/IReactiveSystem.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Entitas {
-    public interface IReactiveSystem : IReactiveExecuteSystem {
-        IMatcher trigger { get; }
-        GroupEventType eventType { get; }
-    }
-}
-
