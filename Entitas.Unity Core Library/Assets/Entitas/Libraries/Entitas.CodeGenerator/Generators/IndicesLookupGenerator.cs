394d691a49d401f02308175206135ea485ff9d02
Dean Lunz <deanlunz@createdbyx.com>
2015-08-26 22:59:57 -0700

    updated git ignore added asset files for core project

---
+++ Entitas.Unity Core Library/Assets/Entitas/Libraries/Entitas.CodeGenerator/Generators/IndicesLookupGenerator.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Entitas.CodeGenerator {
+    public class IndicesLookupGenerator : IComponentCodeGenerator, IPoolCodeGenerator {
+
+        public CodeGenFile[] Generate(Type[] components) {
+            var sortedComponents = components.OrderBy(type => type.ToString()).ToArray();
+            return getLookups(sortedComponents)
+                .Aggregate(new List<CodeGenFile>(), (files, lookup) => {
+                    files.Add(new CodeGenFile {
+                        fileName = lookup.Key,
+                        fileContent = generateIndicesLookup(lookup.Key, lookup.Value.ToArray())
+                    });
+                    return files;
+                }).ToArray();
+        }
+
+        public CodeGenFile[] Generate(string[] poolNames) {
+            var noTypes = new Type[0];
+            if (poolNames.Length == 0) {
+                poolNames = new [] { string.Empty };
+            }
+            return poolNames
+                .Aggregate(new List<CodeGenFile>(), (files, poolName) => {
+                    var lookupTag = poolName + CodeGenerator.defaultIndicesLookupTag;
+                    files.Add(new CodeGenFile {
+                        fileName = lookupTag,
+                        fileContent = generateIndicesLookup(lookupTag, noTypes)
+                    });
+                    return files;
+                }).ToArray();
+        }
+
+        static Dictionary<string, List<Type>> getLookups(Type[] components) {
+            return components
+                .Where(shouldGenerate)
+                .Aggregate(new Dictionary<string, List<Type>>(), (lookups, type) => {
+                    var lookupTag = type.IndicesLookupTag();
+                    if (!lookups.ContainsKey(lookupTag)) {
+                        lookups.Add(lookupTag, new List<Type>());
+                    }
+                        
+                    lookups[lookupTag].Add(type);
+                    return lookups;
+                });
+        }
+
+        static bool shouldGenerate(Type type) {
+            return Attribute.GetCustomAttributes(type)
+                .OfType<DontGenerateAttribute>()
+                .All(attr => attr.generateIndex);
+        }
+
+        static string generateIndicesLookup(string tag, Type[] components) {
+            return addClassHeader(tag)
+                    + addIndices(components)
+                    + addIdToString(components)
+                    + addCloseClass()
+                    + addMatcher(tag);
+        }
+
+        static string addClassHeader(string lookupTag) {
+            var code = string.Format("public static class {0} {{\n", lookupTag);
+            if (stripDefaultTag(lookupTag) != string.Empty) {
+                code = "using Entitas;\n\n" + code;
+            }
+            return code;
+        }
+
+        static string addIndices(Type[] components) {
+            const string fieldFormat = "    public const int {0} = {1};\n";
+            const string totalFormat = "    public const int TotalComponents = {0};";
+            var code = string.Empty;
+            for (int i = 0; i < components.Length; i++) {
+                code += string.Format(fieldFormat, components[i].RemoveComponentSuffix(), i);
+            }
+
+            return code + "\n" + string.Format(totalFormat, components.Length);
+        }
+
+        static string addIdToString(Type[] components) {
+            const string format = "        \"{1}\",\n";
+            const string formatLast = "        \"{1}\"\n";
+            var code = string.Empty;
+            for (int i = 0; i < components.Length; i++) {
+                if (i < components.Length - 1) {
+                    code += string.Format(format, i, components[i].RemoveComponentSuffix());
+                } else {
+                    code += string.Format(formatLast, i, components[i].RemoveComponentSuffix());
+                }
+            }
+
+            return string.Format(@"
+
+    static readonly string[] components = {{
+{0}    }};
+
+    public static string IdToString(int componentId) {{
+        return components[componentId];
+    }}", code);
+        }
+
+        static string addCloseClass() {
+            return "\n}";
+        }
+
+        static string addMatcher(string lookupTag) {
+            var tag = stripDefaultTag(lookupTag);
+            if (tag == string.Empty) {
+                return @"
+
+namespace Entitas {
+    public partial class Matcher : AllOfMatcher {
+        public Matcher(int index) : base(new [] { index }) {
+        }
+
+        public override string ToString() {
+            return " + CodeGenerator.defaultIndicesLookupTag + @".IdToString(indices[0]);
+        }
+    }
+}";
+            }
+
+            return string.Format(@"
+
+public partial class {0}Matcher : AllOfMatcher {{
+    public {0}Matcher(int index) : base(new [] {{ index }}) {{
+    }}
+
+    public override string ToString() {{
+        return {0}" + CodeGenerator.defaultIndicesLookupTag + @".IdToString(indices[0]);
+    }}
+}}", tag);
+        }
+
+        static string stripDefaultTag(string tag) {
+            return tag.Replace(typeof(object).IndicesLookupTag(), string.Empty);
+        }
+    }
+}
---
+++ Entitas.Unity Core Library/Assets/Entitas/Libraries/Entitas/Pool.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Entitas {
+    public partial class Pool {
+
+        public event PoolChanged OnEntityCreated;
+        public event PoolChanged OnEntityWillBeDestroyed;
+        public event PoolChanged OnEntityDestroyed;
+        public event GroupChanged OnGroupCreated;
+
+        public delegate void PoolChanged(Pool pool, Entity entity);
+        public delegate void GroupChanged(Pool pool, Group group);
+
+        public int totalComponents { get { return _totalComponents; } }
+        public int Count { get { return _entities.Count; } }
+        public int pooledEntitiesCount { get { return _entityPool.Count; } }
+
+        protected readonly HashSet<Entity> _entities = new HashSet<Entity>(EntityEqualityComparer.comparer);
+        protected readonly Dictionary<IMatcher, Group> _groups = new Dictionary<IMatcher, Group>();
+        protected readonly List<Group>[] _groupsForIndex;
+        readonly Stack<Entity> _entityPool = new Stack<Entity>();
+        readonly int _totalComponents;
+        int _creationIndex;
+        Entity[] _entitiesCache;
+
+        public Pool(int totalComponents) : this(totalComponents, 0) {
+        }
+
+        public Pool(int totalComponents, int startCreationIndex) {
+            _totalComponents = totalComponents;
+            _creationIndex = startCreationIndex;
+            _groupsForIndex = new List<Group>[totalComponents];
+            _entityPool = new Stack<Entity>();
+        }
+
+        public virtual Entity CreateEntity() {
+            var entity = _entityPool.Count > 0 ? _entityPool.Pop() : new Entity(_totalComponents);
+            entity._isEnabled = true;
+            entity._creationIndex = _creationIndex++;
+            _entities.Add(entity);
+            _entitiesCache = null;
+            entity.OnComponentAdded += onComponentAddedOrRemoved;
+            entity.OnComponentReplaced += onComponentReplaced;
+            entity.OnComponentRemoved += onComponentAddedOrRemoved;
+
+            if (OnEntityCreated != null) {
+                OnEntityCreated(this, entity);
+            }
+
+            return entity;
+        }
+
+        public virtual void DestroyEntity(Entity entity) {
+            var removed = _entities.Remove(entity);
+            if (!removed) {
+                throw new PoolDoesNotContainEntityException(entity,
+                    "Could not destroy entity!");
+            }
+            _entitiesCache = null;
+
+            if (OnEntityWillBeDestroyed != null) {
+                OnEntityWillBeDestroyed(this, entity);
+            }
+
+            entity.RemoveAllComponents();
+            entity.OnComponentAdded -= onComponentAddedOrRemoved;
+            entity.OnComponentReplaced -= onComponentReplaced;
+            entity.OnComponentRemoved -= onComponentAddedOrRemoved;
+            entity._isEnabled = false;
+            _entityPool.Push(entity);
+
+            if (OnEntityDestroyed != null) {
+                OnEntityDestroyed(this, entity);
+            }
+        }
+
+        public virtual void DestroyAllEntities() {
+            var entities = GetEntities();
+            for (int i = 0, entitiesLength = entities.Length; i < entitiesLength; i++) {
+                DestroyEntity(entities[i]);
+            }
+        }
+
+        public virtual bool HasEntity(Entity entity) {
+            return _entities.Contains(entity);
+        }
+
+        public virtual Entity[] GetEntities() {
+            if (_entitiesCache == null) {
+                _entitiesCache = new Entity[_entities.Count];
+                _entities.CopyTo(_entitiesCache);
+            }
+
+            return _entitiesCache;
+        }
+
+        public virtual Group GetGroup(IMatcher matcher) {
+            Group group;
+            if (!_groups.TryGetValue(matcher, out group)) {
+                group = new Group(matcher);
+                var entities = GetEntities();
+                for (int i = 0, entitiesLength = entities.Length; i < entitiesLength; i++) {
+                    group.HandleEntity(entities[i]);
+                }
+                _groups.Add(matcher, group);
+
+                for (int i = 0, indicesLength = matcher.indices.Length; i < indicesLength; i++) {
+                    var index = matcher.indices[i];
+                    if (_groupsForIndex[index] == null) {
+                        _groupsForIndex[index] = new List<Group>();
+                    }
+                    _groupsForIndex[index].Add(group);
+                }
+
+                if (OnGroupCreated != null) {
+                    OnGroupCreated(this, group);
+                }
+            }
+
+            return group;
+        }
+
+        protected void onComponentAddedOrRemoved(Entity entity, int index, IComponent component) {
+            var groups = _groupsForIndex[index];
+            if (groups != null) {
+                for (int i = 0, groupsCount = groups.Count; i < groupsCount; i++) {
+                    groups[i].HandleEntity(entity, index, component);
+                }
+            }
+        }
+
+        protected void onComponentReplaced(Entity entity, int index, IComponent previousComponent, IComponent newComponent) {
+            var groups = _groupsForIndex[index];
+            if (groups != null) {
+                for (int i = 0, groupsCount = groups.Count; i < groupsCount; i++) {
+                    groups[i].UpdateEntity(entity, index, previousComponent, newComponent);
+                }
+            }
+        }
+    }
+
+    public class PoolDoesNotContainEntityException : Exception {
+        public PoolDoesNotContainEntityException(Entity entity, string message) :
+            base(message + "\nPool does not contain entity " + entity) {
+        }
+    }
+}
+
